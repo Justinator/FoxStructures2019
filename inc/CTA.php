e8dcf36c4d8e0a47c080bb6432747a06dad560f3
Justin-Insight <jparsons@insightcreative.com>
2019-02-27 16:33:03 -0600

    continued building out homepage for the site, started editing for mobile devices, added privacy policy and sitemap pages

---
+++ inc/CTA.php
@@ -0,0 +1,25 @@
+<div class="siteCTAWrap">
+
+  <div class="CTAWrapOverlay">
+
+      <div class="CTAcontent">
+
+        <h3 class="whiteText">Fox Structures Careers</h3>
+
+          <p class="whiteText">
+
+            Looking for a new challenge?
+
+          </p>
+
+          <div class="centeredButton">
+
+            <a href="/contact/" class="primaryButton" role="link">Join the team</a>
+
+          </div>
+
+      </div>
+
+  </div>
+
+</div>
