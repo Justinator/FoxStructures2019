95d18726298e346e04d3fa159d3ae92ae22651b5
Justin-Insight <jparsons@insightcreative.com>
2019-05-09 15:02:24 -0500

    added hotjar tracking code to header, added new hidden hollow project to get ready to add that one to the site

---
+++ page-hidden-hollow.php
@@ -0,0 +1,344 @@
+<?php
+/**
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package foxStructuresResponsive
+ */
+get_header();
+
+?>
+
+<div id="primary" class="content-area">
+
+	<main id="main" class="site-main">
+
+		<section class="pageWidth paddedSection limitPortfolioWidth">
+
+			<div class="centerText">
+
+				<h1 class="centerText portfolioTitle">Hidden Hollow ART</h1>
+
+				<div class="centerUnderline"></div>
+
+				<a href="/services/commercial/" class="primaryLink">Commercial</a>
+
+			</div>
+
+			<img
+			sizes="(max-width: 1600px) 100vw, 1600px"
+      srcset="
+        /wp-content/uploads/2019/05/hidden-hollow-outdoor-sign-large-optimized-w_400.jpg 400w,
+				/wp-content/uploads/2019/05/hidden-hollow-outdoor-sign-large-optimized-w_619.jpg 619w,
+				/wp-content/uploads/2019/05/hidden-hollow-outdoor-sign-large-optimized-w_793.jpg 793w,
+				/wp-content/uploads/2019/05/hidden-hollow-outdoor-sign-large-optimized-w_947.jpg 947w,
+        /wp-content/uploads/2019/05/hidden-hollow-outdoor-sign-large-optimized-w_1600.jpg 1600w"
+			src="/wp-content/uploads/2019/05/hidden-hollow-outdoor-sign-large-low.jpg" class="image portfolioFeaturedImage"
+			alt="Hidden Hollow ART exterior sign">
+
+			<p>
+				Fox Structures helped bring to life the whimsical vision for Hidden Hollow
+				ART’s retail space, or as they call it, “Inspiration Destination.” As a
+				hand-crafted metal/wood art shop, the stand-alone building reflects the
+				company’s eclectic character with a unique combination of materials. Stand
+				out features include a gorgeous corrugated steel, standing seam roof and
+				the artist’s custom-made rusty steel shingle pieces. A beautiful I-beam
+				loft served as a perfect canvas for the artists to add their own colorful
+				railings.
+			</p>
+
+		</section>
+
+		<section id="pageContent" class="limitPortfolioWidth">
+
+			<section class="project">
+
+				<div class="fullWidth flex-container">
+
+					<div class="col30">
+
+						<h4 class="noMargin">Project Features</h4>
+
+						<div class="underline"></div>
+
+						<ul>
+
+							<li>I-beam interior loft</li>
+							<li>Corrugated steel roof</li>
+							<li>Custom art installations</li>
+
+						</ul>
+
+					</div>
+
+					<div class="col30">
+
+						<h4 class="noMargin">Specs</h4>
+
+						<div class="underline"></div>
+
+						<ul>
+
+							<li>30’ x 50’ retail space</li>
+							<li>16’ ceilings</li>
+
+						</ul>
+
+					</div>
+
+					<div class="col30">
+
+						<h4 class="noMargin">Location</h4>
+
+						<div class="underline"></div>
+
+						<p>
+							N1833 Orchard Road<br>
+							New Holstein, WI
+						</p>
+
+					</div>
+
+				</div>
+
+				<div class="fullWidth centerText greyBlock smallPaddedSection">
+
+					<div class="testimonial">
+
+						<p>
+
+							“When we first met with Mike and Alex we felt like they were open
+							and up to the challenge of creating a space with a “wow” factor!
+							If we had a question about anything at all, Fox Structures was
+							awesome at explaining why/how things on the project could be done.
+							If we needed something changed, they were accommodating. The whole
+							crew was completely amazing—down to Earth and hardworking. We would
+							absolutely recommend Fox Structures. Their attention to detail and
+							flexibility on design was on point.”
+						</p>
+
+						<h5 class="noMargin">
+						-	Jaimie Danes
+						</h5>
+
+						<div class="centerUnderline"></div>
+
+						<p>
+						Owner, Hidden Hollow ART
+						</p>
+
+					</div>
+
+				</div>
+
+				<div class="flex-container paddedWrapper marginTop">
+
+					<div class="col50 smallMarginRight">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+			      srcset="
+			        /wp-content/uploads/2019/05/hidden-hollow-outdoor-view-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-outdoor-view-optimized-w_655.jpg 655w,
+			        /wp-content/uploads/2019/05/hidden-hollow-outdoor-view-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-outdoor-view-1.jpg" class="image"
+						alt="Exterior view of Hidden hollow ART, commercial build by Fox Structures">
+
+					</div>
+
+					<div class="col50 smallMarginLeft">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+			      srcset="
+			        /wp-content/uploads/2019/05/hidden-hollow-outdoor-view-2-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-outdoor-view-2-optimized-w_641.jpg 641w,
+			        /wp-content/uploads/2019/05/hidden-hollow-outdoor-view-2-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-outdoor-view-2.jpg" class="image"
+						alt="Exterior view of Hidden hollow ART, commercial build by Fox Structures">
+
+					</div>
+
+				</div>
+
+				<div class="flex-container paddedWrapper removeBottomMargin">
+
+					<div class="col50 smallMarginRight">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+			      srcset="
+			        /wp-content/uploads/2019/05/hidden-hollow-art-studio-view-3-low-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-3-low-optimized-w_562.jpg 562w,
+							/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-3-low-optimized-w_714.jpg 714w,
+			        /wp-content/uploads/2019/05/hidden-hollow-art-studio-view-3-low-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-3-low.jpg" class="image"
+						alt="Interior studio view of Hidden Hollow ART">
+
+					</div>
+
+					<div class="col50 smallMarginLeft">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+			      srcset="
+			        /wp-content/uploads/2019/05/hidden-hollow-art-studio-view-2-low-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-2-optimized-w_580.jpg 580w,
+							/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-2-optimized-w_747.jpg 747w,
+			        /wp-content/uploads/2019/05/hidden-hollow-art-studio-view-2-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-2-low.jpg" class="image"
+						alt="Interior studio view of Hidden Hollow ART">
+
+					</div>
+
+				</div>
+
+				<div class="flex-container paddedWrapper removeBottomMargin">
+
+					<div class="col50 smallMarginRight">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+			      srcset="
+			        /wp-content/uploads/2019/05/hidden-hollow-side-room-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-side-room-optimized-w_571.jpg 571w,
+							/wp-content/uploads/2019/05/hidden-hollow-side-room-optimized-w_733.jpg 733w,
+			        /wp-content/uploads/2019/05/hidden-hollow-side-room-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-side-room-low.jpg" class="image"
+						alt="Side room view of Hidden Hollow ART">
+
+					</div>
+
+					<div class="col50 smallMarginLeft">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+			      srcset="
+			        /wp-content/uploads/2019/05/hidden-hollow-art-studio-view-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-optimized-w_565.jpg 565w,
+							/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-optimized-w_719.jpg 719w,
+			        /wp-content/uploads/2019/05/hidden-hollow-art-studio-view-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-art-studio-view-low.jpg" class="image"
+						alt="Interior studio view of Hidden Hollow ART">
+
+					</div>
+
+				</div>
+
+				<div class="flex-container paddedWrapper">
+
+					<div class="col50 smallMarginRight">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+			      srcset="
+			        /wp-content/uploads/2019/05/hidden-hollow-studio-view-desk-balcony-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-studio-view-desk-balcony-optimized-w_525.jpg 525w,
+							/wp-content/uploads/2019/05/hidden-hollow-studio-view-desk-balcony-optimized-w_635.jpg 635w,
+							/wp-content/uploads/2019/05/hidden-hollow-studio-view-desk-balcony-optimized-w_728.jpg 728w,
+			        /wp-content/uploads/2019/05/hidden-hollow-studio-view-desk-balcony-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-studio-view-desk-balcony.jpg" class="image"
+						alt="View of balcony and desk at hidden hollow ART">
+
+					</div>
+
+					<div class="col50 smallMarginLeft removeBottomMarginMobile">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+			      srcset="
+			        /wp-content/uploads/2019/05/hidden-hollow-balcony-view-2-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-balcony-view-2-optimized-w_533.jpg 533w,
+							/wp-content/uploads/2019/05/hidden-hollow-balcony-view-2-optimized-w_657.jpg 657w,
+							/wp-content/uploads/2019/05/hidden-hollow-balcony-view-2-optimized-w_764.jpg 764w,
+			        /wp-content/uploads/2019/05/hidden-hollow-balcony-view-2-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-balcony-view-2-low.jpg" class="image"
+						alt="Balcony view of Hidden Hollow ART">
+
+					</div>
+
+				</div>
+
+				<div class="tinyPaddedSection paddedSideWrapper">
+
+					<img
+					sizes="(max-width: 1600px) 100vw, 1600px"
+					srcset="
+						/wp-content/uploads/2019/05/hidden-hollow-upstairs-balcony-view-1-optimized-w_400.jpg 400w,
+						/wp-content/uploads/2019/05/hidden-hollow-upstairs-balcony-view-1-optimized-w_634.jpg 634w,
+						/wp-content/uploads/2019/05/hidden-hollow-upstairs-balcony-view-1-optimized-w_822.jpg 822w,
+						/wp-content/uploads/2019/05/hidden-hollow-upstairs-balcony-view-1-optimized-w_1000.jpg 1000w,
+						/wp-content/uploads/2019/05/hidden-hollow-upstairs-balcony-view-1-optimized-w_1200.jpg 1200w"
+					src="/wp-content/uploads/2019/05/hidden-hollow-upstairs-balcony-view-1.jpg" class="image largePortfolioImage"
+					alt="Balcony view of Hidden Hollow ART">
+
+				</div>
+
+				<div class="flex-container paddedWrapper removeBottomMargin">
+
+					<div class="col50 smallMarginRight">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+						srcset="
+							/wp-content/uploads/2019/05/hidden-hollow-entryway-view-balcony-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-entryway-view-balcony-optimized-w_607.jpg 607w,
+							/wp-content/uploads/2019/05/hidden-hollow-entryway-view-balcony-optimized-w_775.jpg 775w,
+							/wp-content/uploads/2019/05/hidden-hollow-entryway-view-balcony-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-entryway-view-balcony.jpg" class="image"
+						alt="Balcony view of the main entryway of Hidden Hollow ART">
+
+					</div>
+
+					<div class="col50 smallMarginLeft">
+
+						<img
+						sizes="(max-width: 800px) 50vw, 800px"
+						srcset="
+							/wp-content/uploads/2019/05/hidden-hollow-bathroom-optimized-w_400.jpg 400w,
+							/wp-content/uploads/2019/05/hidden-hollow-bathroom-optimized-w_617.jpg 617w,
+							/wp-content/uploads/2019/05/hidden-hollow-bathroom-optimized-w_791.jpg 791w,
+							/wp-content/uploads/2019/05/hidden-hollow-bathroom-optimized-w_800.jpg 800w"
+						src="/wp-content/uploads/2019/05/hidden-hollow-bathroom.jpg" class="image"
+						alt="Decorated bathroom of Hidden Hollow ART">
+
+					</div>
+
+				</div>
+
+			</section>
+
+			<section class="moreFeaturedProjects smallPaddedSection">
+
+				<div class="pageWidth">
+
+					<h3 class="centerText noMargin">More Featured Projects</h3>
+
+					<div class="centerUnderline"></div>
+
+				</div>
+
+				<div class="moreFeaturedProjectsWrapper">
+
+					<?php get_template_part("/inc/projects/commercial/the-oaks"); ?>
+
+					<?php get_template_part("/inc/projects/commercial/water-right"); ?>
+
+					<?php get_template_part("/inc/projects/agricultural/forest-brook-farms"); ?>
+
+				</div>
+
+			</section>
+
+		</section>
+
+	</main>
+
+</div>
+
+<script>
+filterSelection("all");
+</script>
+
+<?php
+
+get_footer();
